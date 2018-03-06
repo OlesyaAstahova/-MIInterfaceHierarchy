2d97162e60f68f97d09b82c628484d13bf0705a2
Олесуа и Юля <astahova.98@gmail.com>
2018-03-06 10:58:05 +0700

    Множественное наследование посредством интерфейсных типов

---
+++ MIInterfaceHierarchy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    interface IDrawable
+    {
+        void Draw();
+    }
+    interface IPrintable
+    {
+        void Draw(); //Возможен конфликт имен
+        void Print();
+    }
+    //Множественное наследование интерфейсов. Ок
+    interface IShape : IDrawable, IPrintable
+    {
+        int GetNumberOfSides();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** MI Interface Hierarchy *****\n");
+            Square mySquare = new Square();
+            IPrintable t=(IPrintable)mySquare;
+            t.Draw();
+            IDrawable p = (IDrawable)mySquare;
+            p.Draw();
+            mySquare.Print();
+            Console.WriteLine(mySquare.GetNumberOfSides());
+            Console.WriteLine("\n");
+
+            Rectangle Rec = new Rectangle();
+            Rec.GetNumberOfSides();
+            Rec.Draw();
+            Rec.Print();
+            Console.WriteLine(Rec.GetNumberOfSides());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MIInterfaceHierarchy/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIInterfaceHierarchy
+{
+    class Square : IShape
+    {
+        //Использование явной реализации для устранения конфликта имен членов
+        void IPrintable.Draw()
+        {
+            Console.WriteLine("Вывод на принтер");
+        }
+        void IDrawable.Draw()
+        {
+            Console.WriteLine("Вывод на экран...");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Печать");
+        }
+        public int GetNumberOfSides()
+        {
+            return 4;
+        }
+    }
+}
